c24880f07221fb52d977720e5cfe9246f399974b
Tobias Salzmann <salzmann@talon.one>
2018-05-07 15:34:39 +0200

    back to eric's decimal lib

--- decimal.go
+++ decimal.go
@@ -1,18 +1,23 @@
 package decimal
 
 import (
-	"github.com/shopspring/decimal"
+	"errors"
+	"strings"
+	"unicode"
+
+	"github.com/ericlagergren/decimal"
+	"github.com/ericlagergren/decimal/math"
 )
 
 type Decimal struct {
-	native decimal.Decimal
+	native decimal.Big
 }
 
-var Zero Decimal = Decimal{decimal.Zero}
+var Zero = New(0, 0)
 
 func New(value int64, scale int32) Decimal {
 	return Decimal{
-		decimal.New(value, scale),
+		*decimal.New(value, int(scale)),
 	}
 }
 
@@ -34,7 +39,7 @@ func NewFromInt32(i int32) Decimal {
 
 func NewFromInt64(i int64) Decimal {
 	return Decimal{
-		decimal.New(i, 0),
+		*decimal.New(i, 0),
 	}
 }
 
@@ -55,16 +60,21 @@ func NewFromUint32(i uint32) Decimal {
 }
 
 func NewFromUint64(i uint64) Decimal {
-	return Decimal{
-		decimal.New(int64(i), 0),
-	}
+	d := decimal.New(0, 0)
+	d.SetUint64(i)
+	return Decimal{*d}
 }
 
 func NewFromString(s string) (Decimal, error) {
-	d, err := decimal.NewFromString(s)
-	return Decimal{
-		d,
-	}, err
+	if !isDecimal(s) {
+		return Decimal{}, errors.New("Invalid decimal")
+	}
+	d := decimal.New(0, 0)
+	_, ok := d.SetString(s)
+	if !ok {
+		return Decimal{}, errors.New("Invalid decimal")
+	}
+	return Decimal{*d}, nil
 }
 func NewFromFloat(f float64) Decimal {
 	return NewFromFloat64(f)
@@ -73,8 +83,10 @@ func NewFromFloat32(f float32) Decimal {
 	return NewFromFloat64(float64(f))
 }
 func NewFromFloat64(f float64) Decimal {
+	d := decimal.New(0, 0)
+	d.SetFloat64(f)
 	return Decimal{
-		decimal.NewFromFloat(f),
+		*d,
 	}
 }
 
@@ -83,51 +95,107 @@ func (d Decimal) String() string {
 }
 
 func (d Decimal) ToInt64() int64 {
-	return d.IntPart()
+	v, _ := d.native.Int64()
+	return v
 }
 
+// IntPart is an alias for ToInt64 that supports the old decimal api
 func (d Decimal) IntPart() int64 {
-	return d.native.IntPart()
+	return d.ToInt64()
 }
 
 func (d Decimal) Add(x Decimal) Decimal {
-	return Decimal{d.native.Add(x.native)}
+	tmp := decimal.New(0, 0)
+	tmp.Add(&d.native, &x.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Sub(x Decimal) Decimal {
-	return Decimal{d.native.Sub(x.native)}
+	tmp := decimal.New(0, 0)
+	tmp.Sub(&d.native, &x.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Cmp(x Decimal) int {
-	return d.native.Cmp(x.native)
+	return d.native.Cmp(&x.native)
 }
 func (d Decimal) Equals(x Decimal) bool {
-	return d.native.Cmp(x.native) == 0
+	return d.Cmp(x) == 0
 }
 
 func (d Decimal) Div(x Decimal) Decimal {
-	return Decimal{d.native.Div(x.native)}
+	tmp := decimal.New(0, 0)
+	tmp.Quo(&d.native, &x.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Mul(x Decimal) Decimal {
-	return Decimal{d.native.Mul(x.native)}
+	tmp := decimal.New(0, 0)
+	tmp.Mul(&d.native, &x.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Mod(x Decimal) Decimal {
-	return Decimal{d.native.Mod(x.native)}
+	tmp := decimal.New(0, 0)
+	tmp.Rem(&d.native, &x.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Floor() Decimal {
-	return Decimal{d.native.Floor()}
+	tmp := decimal.New(0, 0)
+	math.Floor(tmp, &d.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Ceil() Decimal {
-	return Decimal{d.native.Ceil()}
+	tmp := decimal.New(0, 0)
+	math.Ceil(tmp, &d.native)
+	return Decimal{*tmp}
 }
 
 func (d Decimal) Round(p int) Decimal {
-	return Decimal{d.native.Round(int32(p))}
+	tmp := decimal.New(0, 0)
+	tmp.Copy(&d.native)
+	tmp.Round(p)
+	return Decimal{*tmp}
 }
 func (d Decimal) Truncate(p int) Decimal {
-	return Decimal{d.native.Truncate(int32(p))}
+	parts := strings.SplitN(d.native.String(), ".", 2)
+	if len(parts) <= 1 {
+		v, _ := NewFromString(parts[0])
+		return v
+	}
+	if p > len(parts[1])-1 {
+		p = len(parts[1])
+	}
+	v, _ := NewFromString(parts[0] + "." + parts[1][:p])
+	return v
+}
+
+func isDecimal(s string) bool {
+	if len(s) <= 0 {
+		return false
+	}
+	runes := []rune(s)
+
+	i := 0
+	if runes[0] == '+' || runes[0] == '-' {
+		i++
+	}
+
+	gotDot := false
+	for ; i < len(runes); i++ {
+		if runes[i] == '.' {
+			if gotDot {
+				return false
+			}
+			gotDot = true
+			continue
+		}
+		if !unicode.IsNumber(runes[i]) {
+			return false
+		}
+	}
+
+	return true
 }
